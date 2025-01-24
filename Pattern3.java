c683d57fc1ee5e680ec3323da2281a51f2db577d
surendra2624 <22bk1a05f2@stpetershyd.com>
2025-01-24 15:24:58 +0530

    first commit

---
+++ Pattern3.java
@@ -0,0 +1,20 @@
+
+public class Pattern3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+for(int r=1,sp=10;r<=5;r++) {
+	for(int k=1;k<sp;k++) {
+		System.out.print(" ");
+	}
+	sp =sp-1;
+	for(int c=1;c<=r;c++) {
+		System.out.print(r+" ");
+	}
+	System.out.println();
+		
+	}
+}
+	}
+
+
